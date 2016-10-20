54b02c4d18cc541b05bba2718a0c74646f3a090d
Ben Howdle <benhowdle89@gmail.com>
2016-10-20 10:13:22 +0100

    insta

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 import got from 'got'
 
-const fetch = async username => await got.head(`http://twitter.com/${username}`)
+const fetch = async username => await got.get(`https://instagram.com/${username}/`)
 const sleep = (s = 0) => new Promise(r => setTimeout(r, s * 1000))
 
 const run = async (username) => {
     const keepChecking = async () => {
         console.log(`Checking @${username}...`)
-        const response = await fetch(username)
-        if(response.headers.status.match(/200/)){
+        let response
+        try {
+            response = await fetch(username)
+        } catch (e) {
+            console.log(e)
+            throw new Error(e)
+        }
+        if(response.body.match(new RegExp(username))){
+            console.log(`Username ${username} taken. Rechecking...`)
             await sleep(2)
             return await keepChecking()
         }
